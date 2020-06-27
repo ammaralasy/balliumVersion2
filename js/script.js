104a0e87da5ab51ae371f57f30dfd88cff9879c3
ammaralasy <ammaralasy@yahoo.com>
2020-06-27 17:42:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ window.onload = () => {
       displayToggle.style.display = "flex";
       var before = CSSRulePlugin.getRule(".navigation:before");
     var sequence = gsap.timeline();
-    sequence.fromTo(".navigation", {height: 0}, {height: 150, duration: 0.5})
+    sequence.fromTo(".navigation", {height: 0}, {height: 180, duration: 0.5})
     .fromTo(before, {opacity: 0}, {opacity: 1, duration: 0.5});
     gsap.fromTo(".navigation a", {opacity: 0}, {opacity: 1, duration: 0.5, delay: 0.5});
     }
